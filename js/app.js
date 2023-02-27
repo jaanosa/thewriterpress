d0d95b31c8bf6ba623a1e15da1f1130186f9a80f
jaanosa11 <58842690+jaanosa11@users.noreply.github.com>
2023-02-27 23:25:49 +0800

    Initial HTML Mockup - added index.html - added necessary css and js file - added imgs

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    // effect: 'fade',
+    // fadeEffect: {
+    //   crossFade: true
+    // }
+});
+
+// const swiper = document.querySelector('.swiper').swiper;
